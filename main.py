3ebce8c81ffc28c36ad9644e9be78a6ecf03968d
daviddlima <dlima.david@gmail.com>
2021-11-28 21:36:26 -1000

    cant login to verizon

---
+++ main.py
@@ -0,0 +1,37 @@
+"""
+Purpose: To automate Venmo requesting money for our Verizon phone plan
+
+Notes:
+    - Uses Selenium & Chrome. Requires Chrome Selenium driver extension
+        - https://sites.google.com/chromium.org/driver/
+        - Mac users need to place in /usr/local/bin/ or add the .exec to path
+    - Must already be logged into Venmo & Verizon websites on Chrome
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from secrets import *
+
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://www.verizonwireless.com/my-verizon/")
+    try:
+        user = WebDriverWait(driver, 25).until(
+            EC.presence_of_element_located((By.NAME, "IDToken1"))
+        )
+        user.clear()
+        user.send_keys(VERIZON_USERNAME)
+        password = driver.find_element(By.NAME, "IDToken2")
+        password.send_keys(VERIZON_PASSWORD)
+        driver.find_element(By.ID, "login-submit").click()
+    except Exception as e:
+        print(e)
+
+    # driver.close()
+
+
+if __name__ == "__main__":
+    main()
